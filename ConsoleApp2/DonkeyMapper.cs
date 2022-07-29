ad49e22eb8adc5887ca386e9b3c7fde427ace4b8
Faerytern <kara.heffernan.nz@gmail.com>
2022-07-29 17:40:43 +1200

    Test

--- ConsoleApp2/DonkeyMapper.cs
+++ ConsoleApp2/DonkeyMapper.cs
@@ -1,5 +1,7 @@
 ﻿using Dapper.FluentMap.Mapping;
 using ConsoleApp2;
+
+// Dapper does technically have it's own custom mapper, but it would be an entire page's worth of code per column to change. The FluentMap extension is super helpful, reducing the required code to just this and registering it before the DB call!
 public class DonkeyMapper : EntityMap<Donkey>
 {
     public DonkeyMapper()
